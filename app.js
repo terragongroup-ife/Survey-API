8fa64075274b2fc276f7694ce7599c77da0c3ff4
Your Name <you@example.com>
2019-03-11 11:30:35 +0100

    edited the app.js file

--- app.js
+++ app.js
@@ -11,16 +11,18 @@ const port = config.server.port;
 const appName = config.appName || "Survey";
 
 // Setting CORS for server conflicts.
-app.use( (req,res, next)=>{
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "*");
-     next();
+// app.use( (req,res, next)=>{
+//     res.header("Access-Control-Allow-Origin", "*");
+//     res.header("Access-Control-Allow-Headers", "*");
+//      next();
 
-    if(req.method === "OPTIONS"){
-        res.header("Acess-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
-        return res.status(200).json({});
-    }
-});
+//     if(req.method === "OPTIONS"){
+//         res.header("Acess-Control-Allow-Methods", "POST, GET, PUT, PATCH, DELETE");
+//         return res.status(200).json({});
+//     }
+// });
+
+app.use(cors());
 
 app.use(router);
 
